0731d5c65172358b8b1aa02ca524809807f23bd6
Abhiek187 <achaudhuri2011@yahoo.com>
2020-12-29 15:03:48 -0500

    Fetch data from MongoDB instead of from memory

--- api/index.js
+++ api/index.js
@@ -1,25 +1,44 @@
-import data from "../src/testData";
 import express from "express";
+import { MongoClient } from "mongodb";
+import assert from "assert";
+import config from "../config";
+
+let mdb; // MongoDB object
+MongoClient.connect(config.mongodbUri, {useUnifiedTopology: true}, (err, client) => {
+	assert.equal(null, err);
+	mdb = client.db("test"); // test is the default database
+});
 
 const router = express.Router();
-// Create a new object with keys of the contest IDs
-const contests = data.contests.reduce((obj, contest) => {
-	obj[contest.id] = contest;
-	return obj;
-}, {});
 
 router.get("/contests", (req, res) => {
-	res.send({
-		contests
-	});
+	let contests = {};
+
+	mdb.collection("contests").find({})
+		.project({
+			id: 1,
+			categoryName: 1,
+			contestName: 1
+		})
+		.each((err, contest) => {
+			assert.equal(null, err);
+
+			if (!contest) {
+				// No more contests (make the key contests)
+				res.send({ contests });
+				return;
+			}
+
+			contests[contest.id] = contest;
+		});
 });
 
 // Get a dynamic contest ID route
 router.get("/contests/:contestId", (req, res) => {
-	const contest = contests[req.params.contestId];
-	contest.description = "Chrysno's compiler error";
-
-	res.send(contest);
+	mdb.collection("contests")
+		.findOne({ id: Number(req.params.contestId) })
+		.then(contest => res.send(contest))
+		.catch(console.error);
 });
 
 export default router;
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const env = process.env; // process = global object
 
 export default {
+	mongodbUri: "mongodb://localhost:27017/test", // test is the default database
 	port: env.PORT || 8080, // default to port 8080
 	host: env.HOST || "0.0.0.0", // bind to all IPs on the machine
 	get serverUrl() {
